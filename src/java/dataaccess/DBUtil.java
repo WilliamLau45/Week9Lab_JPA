5b54f62af0d524a197bf02ad31ed72b7ac0f9d89
ohiow <ohiow@10.0.0.187>
2022-11-06 22:41:06 -0700

    Edited Week9Lab Remove connectionPool, updated DBUtil for JPA

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -1,25 +1,13 @@
 package dataaccess;
 
-import java.sql.*;
-
-public class DBUtil {
-    public static void closePreparedStatement(Statement ps) {
-        try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
-
-    public static void closeResultSet(ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-    }
+	import javax.persistence.EntityManagerFactory;
+	import javax.persistence.Persistence;
+	 
+	public class DBUtil {
+	    private static final EntityManagerFactory emf =
+	        Persistence.createEntityManagerFactory("Week9Lab_JPAPU");
+	
+	    public static EntityManagerFactory getEmFactory() {
+	        return emf;
+	    }
 }
